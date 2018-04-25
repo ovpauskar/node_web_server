d3190a31c30aff2daab5e2416ef1a8090878201c
Omkar <ovpauskar@gmail.com>
2018-04-26 02:31:09 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) =>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n" ,(err)=>{
+    if(err){
+      console.log("unable to append server.log file");
+     }
+  });
+  next();
+});
+
+hbs.registerHelper("screamIt", (text) =>{
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) =>{
+  res.render("home.hbs", {
+    title: "Home Page",
+    welcomeMessage: "This is home page!",
+    currentyear: new Date().getFullYear()
+  });
+});
+
+app.get("/about",(req, res) =>{
+  res.render("about.hbs", {
+    page_title: "About page",
+    currentyear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log("server Started!");
+});
